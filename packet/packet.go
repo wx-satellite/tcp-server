05eaf5695ec05ea0eaa38dd864b0aef1be798d5e
wxsatellite <1453085314@qq.com>
2022-04-22 13:54:24 +0800

    fix

--- packet/packet.go
+++ packet/packet.go
@@ -18,8 +18,7 @@ body：
 ack body：
 	8字节：ID
 	1字节：result
- */
-
+*/
 
 const (
 	CommandConn = iota + 0x01
@@ -33,45 +32,42 @@ const (
 
 type Packet interface {
 	Decode([]byte) error
-	Encode()([]byte,error)
+	Encode() ([]byte, error)
 }
 
-
 type Submit struct {
-	ID string
+	ID      string
 	Payload []byte
 }
 
-func (s*Submit)Decode(body []byte)(err error) {
+func (s *Submit) Decode(body []byte) (err error) {
 	s.ID = string(body[:8])
 	s.Payload = body[9:]
 	return
 }
 
-func (s *Submit) Encode()(body []byte, err error) {
-	body = bytes.Join([][]byte{[]byte(s.ID)[:8],s.Payload},nil)
+func (s *Submit) Encode() (body []byte, err error) {
+	body = bytes.Join([][]byte{[]byte(s.ID)[:8], s.Payload}, nil)
 	return
 }
 
-
 type SubmitAck struct {
-	ID string
+	ID     string
 	Result uint8 // byte 就是 uint8 的别名
 }
 
-func (s *SubmitAck) Decode(body []byte)(err error) {
+func (s *SubmitAck) Decode(body []byte) (err error) {
 	s.ID = string(body[:8])
 	s.Result = body[8]
 	return
 }
 
-func (s *SubmitAck) Encode()(body []byte, err error) {
-	body = bytes.Join([][]byte{[]byte(s.ID),{s.Result}},nil)
+func (s *SubmitAck) Encode() (body []byte, err error) {
+	body = bytes.Join([][]byte{[]byte(s.ID), {s.Result}}, nil)
 	return
 }
 
-
-func Decode(packet []byte)(p Packet, err error) {
+func Decode(packet []byte) (p Packet, err error) {
 	header := packet[0]
 	body := packet[1:]
 	switch header {
@@ -101,20 +97,20 @@ func Decode(packet []byte)(p Packet, err error) {
 	}
 }
 
-func Encode(p Packet)(packet []byte, err error) {
+func Encode(p Packet) (packet []byte, err error) {
 	var (
 		header uint8
-		body []byte
+		body   []byte
 	)
 	switch v := p.(type) {
 	case *SubmitAck:
-		header = CommandSubmit
-		body, err= v.Encode()
+		header = CommandSubmitAck
+		body, err = v.Encode()
 		if err != nil {
 			return
 		}
 	case *Submit:
-		header = CommandSubmitAck
+		header = CommandSubmit
 		body, err = v.Encode()
 		if err != nil {
 			return
@@ -123,6 +119,6 @@ func Encode(p Packet)(packet []byte, err error) {
 		err = fmt.Errorf("unkown type")
 		return
 	}
-	packet = bytes.Join([][]byte{{header},body},nil)
+	packet = bytes.Join([][]byte{{header}, body}, nil)
 	return
 }
